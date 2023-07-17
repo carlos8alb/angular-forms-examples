45f33369b1f9d18be843fd10b928a1312c44157f
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-07-17 20:25:37 -0300

    feat: add components

---
+++ src/app/pages/reactive/reactive.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reactive',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reactive.component.html',
+  styleUrls: ['./reactive.component.css']
+})
+export class ReactiveComponent {
+
+}
---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-template',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent {
+
+}
