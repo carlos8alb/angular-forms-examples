164c996ddce4a0b2d33b474e08f998bbf4bfe788
Carlos Albarracin <carlos8_alb@hotmail.com>
2023-07-18 17:15:34 -0300

    feat: finish template form example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CountryService } from 'src/app/services/country.service';
+import { CountryResults } from 'src/app/types/types';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-template',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  styleUrls: ['./template.component.css'],
 })
-export class TemplateComponent {
+export class TemplateComponent implements OnInit {
+  countryService = inject(CountryService);
 
+  user = {
+    name: 'Carlos',
+    lastname: 'Albarracin',
+    email: 'carlos8_alb@hotmail.com',
+    country: 'ARG',
+    gender: 'M'
+  };
+
+  emailPattern = '[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$';
+
+  countries$!: Observable<any[]>;
+  defaultOption: CountryResults[] = [{ name: '[Select a country]', code: '' }]
+
+  ngOnInit(): void {
+    this.countries$ = this.countryService
+      .getCountries()
+      .pipe(
+        map((countries: CountryResults[]) => this.defaultOption.concat(countries))
+      );
+  }
+
+  save(form: NgForm): void {
+    Object.values(form.controls).forEach((control) => {
+      control.markAsTouched();
+    });
+
+    if (form.invalid) return;
+
+    console.log(form.value);
+  }
 }
